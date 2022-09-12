b6ebead86ce29cb57d587fde57f2fa9d960d7670
Right Geek <rgeekdigital@gmail.com>
2022-09-12 13:10:00 +0100

    ...

--- src/landing.js
+++ src/landing.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 export default function Landing() {
     return (
-        <h1>Quizical App</h1>
-        <h4>Welcome!</h4>
+        <>
+            <h1>Quizzical App</h1>
+            <h4>Welcome!</h4>
+        </>
     )
 }
